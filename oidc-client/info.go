00d78ee59e0528f982b685f2c513d8f9a5021571
vdbulcke <cedric.vandenbulcke.linux@gmail.com>
2022-01-24 17:40:49 +0100

    fix: Remove ignored Userinfo  references #5

--- oidc-client/info.go
+++ oidc-client/info.go
@@ -10,7 +10,7 @@ func (c *OIDCClient) Info() {
 
 	conf := fmt.Sprintf("ClientID: %s\nClient_Secret: %s\nRedirect_Uri: %s\nScopes: %s\nIssuer: %s", c.config.ClientID, "************", c.config.RedirectUri, strings.Join(c.config.Scopes, ","), c.config.Issuer)
 
-	advanced := fmt.Sprintf("AcrValues: %s\nTokenEndpoint: %s\nAuthorizeEndpoint: %s\nUserinfoEndpoint: %s\nTokenSigningAlg: %s\nSkipTLSVerification: %t\nJwksEndpoint: %s", c.config.AcrValues, c.config.TokenEndpoint, c.config.AuthorizeEndpoint, c.config.UserinfoEndpoint, c.config.TokenSigningAlg, c.config.SkipTLSVerification, c.config.JwksEndpoint)
+	advanced := fmt.Sprintf("AcrValues: %s\nTokenEndpoint: %s\nAuthorizeEndpoint: %s\nTokenSigningAlg: %s\nSkipTLSVerification: %t\nJwksEndpoint: %s", c.config.AcrValues, c.config.TokenEndpoint, c.config.AuthorizeEndpoint, c.config.TokenSigningAlg, c.config.SkipTLSVerification, c.config.JwksEndpoint)
 
 	c.logger.Info("OIDC Client", "Config", conf, "Advanced", advanced, "Amrs", c.config.AMRWhitelist)
 }
