5b4d7ddd3c5dcea8513c840830e942aa209b10e9
vdbulcke <cedric.vandenbulcke.linux@gmail.com>
2024-04-12 17:31:37 +0200

    fix: req body parsing (#60)

--- src/client/http/transport.go
+++ src/client/http/transport.go
@@ -32,22 +32,21 @@ func (t *LogTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	reqId := uuid.New().String()
 	start := time.Now()
 
-	reqBody := []byte("nil")
-	if req.Body != nil {
-
-		reqBody, err := io.ReadAll(req.Body)
-		if err == nil {
+	if t.logger.IsDebug() {
+		reqBody := []byte("nil")
+		var err error
+		if req.Body != nil {
+			reqBody, err = io.ReadAll(req.Body)
+			if err != nil {
+				t.logger.Error("error parsing req body", "err", err)
+			}
 			// don't close body here
 			req.Body = io.NopCloser(bytes.NewBuffer(reqBody))
-
 		}
+
+		reqStr := fmt.Sprintf("Method: %s\nUrl: %s\nHost: %s\nHeaders: %#v\nBody: %s", req.Method, req.URL, req.Host, req.Header, string(reqBody))
+		t.logger.Debug("http.client request", "request_id", reqId, "req", reqStr)
 	}
-
-	//nolint
-	req.ParseForm()
-
-	reqStr := fmt.Sprintf("Method: %s\nUrl: %s\nHeaders: %#v\nForm: %#v\nBody: %s", req.Method, req.URL, req.Header, req.Form, string(reqBody))
-	t.logger.Debug("http.client request", "request_id", reqId, "req", reqStr)
 
 	resp, err := t.roundTripper.RoundTrip(req)
 	if err != nil {
@@ -56,16 +55,18 @@ func (t *LogTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 
 	elasped_time := time.Since(start)
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
+	if t.logger.IsDebug() {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
 
-		return resp, err
+			return resp, err
+		}
+
+		// don't close body here
+		resp.Body = io.NopCloser(bytes.NewBuffer(body))
+
+		respStr := fmt.Sprintf("Status: %s\nHeaders: %#v\nBody: %s", resp.Status, resp.Header, string(body))
+		t.logger.Debug("http.client response", "request_id", reqId, "esplased", elasped_time, "resp", respStr)
 	}
-
-	// don't close body here
-	resp.Body = io.NopCloser(bytes.NewBuffer(body))
-
-	respStr := fmt.Sprintf("Status: %s\nHeaders: %#v\nBody: %s", resp.Status, resp.Header, string(body))
-	t.logger.Debug("http.client response", "request_id", reqId, "esplased", elasped_time, "resp", respStr)
 	return resp, err
 }
